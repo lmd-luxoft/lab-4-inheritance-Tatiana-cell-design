cb81fcf2c8e848de77079b4ecc899c81d3827d02
Tatiana Khodataeva <tatiana.khodataeva@dxc.com>
2021-05-13 19:19:39 +0300

    Замена switch case паттерном стратегия

--- Chess/ChessFigure.cs
+++ Chess/ChessFigure.cs
@@ -5,103 +5,130 @@ namespace Chess
 {
     public class ChessFigure
     {
-        private Type type;
         private string currentCoord;
+        public IMoveFigure FavoriteMoveFigure { get; private set; }
 
-        public ChessFigure(Type type, string currentCoord)
+        public ChessFigure(string currentCoord,
+                            IMoveFigure favoriteMoveFigure)
+
         {
-            this.type = type;
             this.currentCoord = currentCoord;
+            FavoriteMoveFigure = favoriteMoveFigure;
+
         }
 
-        public enum Type
+        /*      public enum Type
+			  {ROOK,KNIGHT,BISHOP,PAWN,KING,QUEEN  }
+		*/
+        public bool CheckField(string nextCoord) {
+               
+              if (nextCoord[0] >= 'A' && nextCoord[0] <= 'H' && nextCoord[1] >= '1' && nextCoord[1] <= '8')
+         
+                return true;
+            else
+                return false;
+        }
+        public bool MoveTo(string nextCoord, IMoveFigure moveFigure)  
         {
-            ROOK,
-            KNIGHT,
-            BISHOP,
-            PAWN,
-            KING,
-            QUEEN
+
+            if (CheckField(nextCoord))
+            {
+                if (moveFigure.Move(nextCoord, currentCoord))
+                    return true;
+                else
+                    return false;
+            }
+            else
+                return false;
         }
 
-        internal bool Move(string nextCoord)
+        public bool MoveTo(string nextCoord)
         {
-			if (type == Type.PAWN)
-			{
-				if (nextCoord[0] >= 'A' && nextCoord[0] <= 'H' && nextCoord[1] >= '1' && nextCoord[1] <= '8')
-				{
-					if (nextCoord[0] != currentCoord[0] || nextCoord[1] <= currentCoord[1] || (nextCoord[1] - currentCoord[1] != 1 && (currentCoord[1] != '2' || nextCoord[1] != '4')))
-						return false;
-					else
-						return true;
-				}
-				else return false;
+            if (CheckField(nextCoord))
+            {
+                if (MoveTo(nextCoord, FavoriteMoveFigure))
+                    return true;
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
 
-			}
+    }
 
-			else if (type == Type.ROOK)
-			{
-				if (nextCoord[0] >= 'A' && nextCoord[0] <= 'H' && nextCoord[1] >= '1' && nextCoord[1] <= '8')
-				{
-					if ((nextCoord[0] != currentCoord[0]) && (nextCoord[1] != currentCoord[1]) || ((nextCoord[0] == currentCoord[0]) && (nextCoord[1] == currentCoord[1])))
-						return false;
-					else
-						return true;
+    public interface IMoveFigure
+    {
+        bool Move(string nextCoord, string currentCoord);              
 
-				}
-				else return false;
-			}
-			else if (type == Type.KNIGHT)
-			{
-				if (nextCoord[0] >= 'A' && nextCoord[0] <= 'H' && nextCoord[1] >= '1' && nextCoord[1] <= '8')
-				{
-					int dx, dy;
-					dx = Math.Abs(nextCoord[0] - currentCoord[0]);
-					dy = Math.Abs(nextCoord[1] - currentCoord[1]);
-					if (!(Math.Abs(nextCoord[0] - currentCoord[0]) == 1 && Math.Abs(nextCoord[1] - currentCoord[1]) == 2 || Math.Abs(nextCoord[0] - currentCoord[0]) == 2 && Math.Abs(nextCoord[1] - currentCoord[1]) == 1))
-						return false;
-					else
-						return true;
-				}
-				else return false;
-			}
+    }
 
-			else if (type == Type.BISHOP)
-			{
-				if (nextCoord[0] >= 'A' && nextCoord[0] <= 'H' && nextCoord[1] >= '1' && nextCoord[1] <= '8')
-				{
-					if (!(Math.Abs(nextCoord[0] - currentCoord[0]) == Math.Abs(nextCoord[1] - currentCoord[1])))
-						return false;
-					else
-						return true;
-				}
-				else return false;
-			}
 
-			else if (type == Type.KING)
-			{
-				if (nextCoord[0] >= 'A' && nextCoord[0] <= 'H' && nextCoord[1] >= '1' && nextCoord[1] <= '8')
-				{
-					if (!(Math.Abs(nextCoord[0] - currentCoord[0]) <= 1 && Math.Abs(nextCoord[1] - currentCoord[1]) <= 1))
-						return false;
-					else
-						return true;
-				}
-				else return false;
-			}
-			else if (type == Type.QUEEN)
-			{
-				if (nextCoord[0] >= 'A' && nextCoord[0] <= 'H' && nextCoord[1] >= '1' && nextCoord[1] <= '8')
-				{
-					if (!(Math.Abs(nextCoord[0] - currentCoord[0]) == Math.Abs(nextCoord[1] - currentCoord[1]) || nextCoord[0] == currentCoord[0] || nextCoord[1] == currentCoord[1]))
-						return false;
-					else
-						return true;
-				}
-				else return false;
-			}
-			else
-				return false;
-		}
+    public class MovePawn : IMoveFigure
+    {
+        public bool Move(string nextCoord, string currentCoord)     
+        {
+            if (nextCoord[0] != currentCoord[0] || nextCoord[1] <= currentCoord[1] || (nextCoord[1] - currentCoord[1] != 1 && (currentCoord[1] != '2' || nextCoord[1] != '4')))
+                return false;
+            else
+                return true;
+        }
+    }
+
+    public class MoveRook : IMoveFigure
+    {
+        public bool Move(string nextCoord, string currentCoord)        
+        {
+            if ((nextCoord[0] != currentCoord[0]) && (nextCoord[1] != currentCoord[1]) || ((nextCoord[0] == currentCoord[0]) && (nextCoord[1] == currentCoord[1])))
+                return false;
+            else
+                return true;
+        }
+    }
+
+    public class MoveKnight : IMoveFigure
+    {
+        public bool Move(string nextCoord, string currentCoord)         
+        {
+            int dx, dy;
+            dx = Math.Abs(nextCoord[0] - currentCoord[0]);
+            dy = Math.Abs(nextCoord[1] - currentCoord[1]);
+            if (!(Math.Abs(nextCoord[0] - currentCoord[0]) == 1 && Math.Abs(nextCoord[1] - currentCoord[1]) == 2 || Math.Abs(nextCoord[0] - currentCoord[0]) == 2 && Math.Abs(nextCoord[1] - currentCoord[1]) == 1))
+                return false;
+            else
+                return true;
+        }
+    }
+
+    public class MoveBishop : IMoveFigure
+    {
+        public bool Move(string nextCoord, string currentCoord)    
+        {
+            if (!(Math.Abs(nextCoord[0] - currentCoord[0]) == Math.Abs(nextCoord[1] - currentCoord[1])))
+                return false;
+            else
+                return true;
+        }
+    }
+    public class MoveKing : IMoveFigure
+    {
+        public bool Move(string nextCoord, string currentCoord)          
+        {
+            if (!(Math.Abs(nextCoord[0] - currentCoord[0]) <= 1 && Math.Abs(nextCoord[1] - currentCoord[1]) <= 1))
+                return false;
+            else
+                return true;
+        }
+    }
+    public class MoveQueen : IMoveFigure
+    {
+        public bool Move(string nextCoord, string currentCoord)          
+
+        {
+            if (!(Math.Abs(nextCoord[0] - currentCoord[0]) == Math.Abs(nextCoord[1] - currentCoord[1]) || nextCoord[0] == currentCoord[0] || nextCoord[1] == currentCoord[1]))
+                return false;
+            else
+                return true;
+        }
     }
 }
